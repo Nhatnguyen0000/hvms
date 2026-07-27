e7fff800e61147d3ca8fa4b22dc65eee55fcace4
cntt <151663314+Nhatnguyen0000@users.noreply.github.com>
2026-07-27 14:41:18 +0700

    Add files via upload

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,10 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_HMAC_SECRET: string;
   readonly VITE_SUPABASE_URL: string;
   readonly VITE_SUPABASE_ANON_KEY: string;
-  readonly VITE_SUPABASE_SERVICE_KEY: string;
   readonly VITE_STORAGE_ENCRYPTION_KEY: string;
-  readonly GEMINI_API_KEY: string;
-  readonly APP_URL: string;
 }
 
 interface ImportMeta {
